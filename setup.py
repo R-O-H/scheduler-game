312fdeefdc2bf106ef95c178647c8f1c8fae53b7
R-O-H <107893063+R-O-H@users.noreply.github.com>
2023-04-21 19:40:55 -0400

    Adds files to build as pacakge or exe

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
-    install_requires=["pygame"],
+    install_requires=["pygame", "console"],
 )
